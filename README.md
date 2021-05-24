b33d8cb981f7f9334c4c4a970a4e490852c848f1
Daniel Hasegan <danielhasegan@gmail.com>
2021-05-24 13:14:56 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# SMARTAgent
+A closed-loop neuronal network model that senses dynamic visual information from the AIgame enviroment and learns to produce actions that maximize game reward through spike-timing dependent reinforcement learning.
+
+### Install on Mac
+
+install python
+create a virtual environment
+
+    virtualenv -p python3 venv
+
+activate the environment
+
+    source ./venv/bin/activate
+
+install all dependencies:
+
+    pip install -r requirements.txt
---
+++ anim.py
@@ -0,0 +1,44 @@
+# utilities for animation output
+import imageio
+import os
+from matplotlib import animation
+
+# save set of images (specified by path) to output animated gif
+def savegif (lfnimage, outpath):
+  limage = [imageio.imread(fn) for fn in lfnimage]
+  imageio.mimwrite(outpath, limage)
+
+# save set of images specified by inpath to output mp4; framerate is frames per second
+# framerate should be >= 10; otherwise ffmpeg may produce buggy output video
+def savemp4 (inpath, outpath, framerate):
+  import ffmpeg # pip install ffmpeg-python
+  if os.path.isfile(outpath): os.unlink(outpath) # make sure can write to this path  
+  (
+    ffmpeg
+    .input(inpath, pattern_type='glob', framerate=framerate)
+    .output(outpath)
+    .run()
+  )
+  
+# ffmpeg.concat(split0, split1).output('out.mp4').run()
+
+def getwriter (outpath, framerate):
+  if outpath.endswith('mp4'):
+    # Either avconv or ffmpeg need to be installed in the system to produce the videos!
+    try:
+      writer = animation.writers['ffmpeg']
+    except KeyError:
+      writer = animation.writers['avconv']
+    writer = writer(fps=framerate)
+    return writer
+    #ani.save(outpath, writer=writer)
+  elif outpath.endswith('gif'):
+    try:
+      writer = animation.writers['imagemagick'] # need to have imagemagick installed
+      writer = writer(fps=framerate)
+      return writer
+      #ani.save(outpath, writer=writer)
+    except:
+      print('imagemagick not available')
+  return None
+  
